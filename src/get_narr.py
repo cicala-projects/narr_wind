1fbf4b18acd1ea8d37ebb6d6ff97c7e0ea91e11a
ivanhigueram <ivanhigueram@gmail.com>
2019-10-29 16:28:10 -0500

    Update downloader gribfiles to s3

--- src/get_narr.py
+++ src/get_narr.py
@@ -4,13 +4,18 @@ Retrieve and store NARR files from the NOAA FTP server
 
 
 import os
+import boto3
 import docker
 import logging
 import requests
 import asyncio
 import aiofiles
 import aiohttp
+import zipfile
+import smart_open
 import async_timeout
+import concurrent.futures
+from bs4 import BeautifulSoup
 from urlpath import URL
 from aiohttp import ClientSession
 from itertools import product
@@ -41,90 +46,40 @@ def datetime_range(start, end, delta):
         current += delta
 
 
-def retrieve_month_async(start_day_date,
-                             save_path):
+def requests_to_s3(base_url,
+                  file_name,
+                  auth):
+
     """
-    Download individual month directory of .grd files to local directory in
-    asynchronous implementation.
-
-    This function will download using the ftplib all the .grd files between the
-    start_date and the end_date. All dates in the NOAA NARR FTP server are
-    stored following this order:
-        data
-        ├── year/month
-            ├── year/month/day01
-            ├── year/month/day02
-
-    Here we download the monthly directory with the user-defined dates in the
-    start and end dates. 
-
-    Params:
-        - start_year str: year to start download.
-        - end_year str: year to stop download.
+    Request (GET) a URL and stream their contents into an s3 bucket
+    using smart_open.
+    This function uses requests to get the contents of a file URL and stream
+    the bytes to a s3 bucket/key. This is made possible by a boto3 session and
+    a utility function that checks that the file does not exist in the bucket
+    already before downloading (the `smart_open` default behavior is
+    overwriting the file, but here we don't want to lose time requesting data). 
+    This function can be use as a stand-alone function, or as a part of a
+    multiprocess call, as in the stream_download_s3_parallel function. 
+    :param str base_url:
     """
 
-    logger = logging.getLogger(__name__)
-
-    if not isinstance(start_day_date, datetime):
-        start_day_date = datetime.strptime(start_day_date, '%Y-%m-%d')
-    else:
-        ValueError(f'{start_day_date} is not in the correct format or not a valid type')
-
-    base_url = "https://nomads.ncdc.noaa.gov/data/narr"
-
-    # Build month grb URL paths
-    time_generators = [datetime_range(start_time, 
-                                     start_time + relativedelta(days=1),
-                                      {'hours': 3}) for start_time in 
-                      datetime_range(start_day_date,
-                                    start_day_date + relativedelta(months=1),
-                                     {'days': 1})
-                      ]
-
-    grib_files_url = [URL(base_url,
-                          date.strftime('%Y%m'),
-                          date.strftime('%Y%m%d'),
-                          f'narr-a_221_{date.strftime("%Y%m%d_%H%M")}_000.grb') 
-                      for generator in time_generators for date in generator]
-
-    async def download_coroutine(session, url):
-        url_obj = URL(url)
-        month_dir, day_dir = (url_obj.parts[3], url_obj.parts[4])
-        path = os.path.join(save_path, month_dir, day_dir)
-        logger.info(f'Starting transmission for {url} files')
-
-        if os.path.exists(path):
-            logger.info('Directory exists! Skip dir creation')
+    headers = {'Authorization': f'Bearer {auth}'}
+    print(f'{file_name}')
+    with requests.get(base_url + file_name, headers=headers) as file_request:
+        if file_request.status_code == requests.codes.ok:
+            return file_request.content
         else:
-            os.makedirs(path)
-
-        async with session.get(url) as response:
-            filepath = os.path.join(path, url_obj.name)
-            async with aiofiles.open(os.path.join(path, url_obj.name), 'wb') as file_stream:
-                while True:
-                    chunk = await response.content.read()
-                    if not chunk:
-                        break
-                    await file_stream.write(chunk)
-            return await response.release()
-
-    async def main(loop, url):
-        sem = asyncio.Semaphore(-1)
-        connector = aiohttp.TCPConnector(limit=60, share_cookies=True)
-        async with aiohttp.ClientSession(loop=loop, connector=connector) as session:
-            async with sem:
-                await download_coroutine(session, url)
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(
-        asyncio.gather(
-            *(main(loop, url.as_uri()) for url in grib_files_url)
-        )
-    )
+            logger.info(f'Request GET failed with {file_request.content} [{file_request.url}]')
 
 
-def retrieve_individual_month(start_date,
-                              save_path):
+
+def stream_time_range_s3(start_date,
+                         end_date,
+                         aws_key,
+                         aws_secret,
+                         aws_bucket_name,
+                         key,
+                         max_workers):
     """
     Download individual month directory of .grd files to local directory.
 
@@ -151,47 +106,39 @@ def retrieve_individual_month(start_date,
     else:
         ValueError(f'{start_date} is not in the correct format or not a valid type')
 
+    session = boto3.Session(
+        aws_access_key_id=aws_key,
+        aws_secret_access_key=aws_secret
+    )
 
-    with FTP('nomads.ncdc.noaa.gov') as ftp:
-        response = ftp.login()
-        if response == '230 Anonymous access granted, restrictions apply':
+    time = ['0000', '0300', '0600', '0900', '1200', '1500', '1800', '2100']
+    dates = datetime_range(start_date, end_date, {'days':1})
 
-            date_str = start_date.strftime('%Y%m')
-            logger.info(f'Starting year/month: {date_str}')
-            try:
-                ftp.cwd(f'NARR/{date_str}')
-                dirnames = ftp.nlst()
-                for day in dirnames:
-                    ftp.cwd(f'{day}')
 
-                    # Select only narr-b files with the measured (not
-                    # predicted) values.
-                    grb_files = [f for f in ftp.nlst() if
-                                 f.startswith('narr-a') and 
-                                 f.endswith('.grb')]
+    urls_time_range = []
+    for day, time in product(dates, time):
+           file_name = f'narr-a_221_{day.strftime("%Y%m%d")}_{time}_000.grb'
+           url = URL(base_url, day.strftime('%Y%m'), day.strftime('%Y%m%d'))
+           urls_time_range.append(URL(url, file_name))
 
-                    for grb in grb_files:
-                        path = os.path.join(save_path, date_str, day)
-                        logger.info(f'Starting transmission for {grb} file')
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        executor_url = {executor.submit(requests_to_s3, 
+                                        url,
+                                        file_name,
+                                        auth): file_name for file_name in urls_time_range}
 
-                        if os.path.exists(path):
-                            logger.info('Directory exists! Skip dir creation')
-                        else:
-                            os.makedirs(path)
+            buf = io.BytesIO()
 
-                        with open(os.path.join(path, grb), 'wb') as file_grb:
-                            ftp.retrbinary(f'RETR {grb}', file_grb.write)
+            with zipfile.ZipFile(buf, mode='w', compression=zipfile.ZIP_DEFLATED) as zf:
+                for content_file in concurrent.futures.as_completed(executor_url):
+                    zf.writestr(executor_url[content_file],
+                               content_file.result())
 
-                    ftp.cwd('..')
-                ftp.cwd('../..')
-            except all_errors as e:
-                logger.debug(f'Oops! Something went wrong. FTP error: {e}')
+            with smart_open.open(key, 'wb', transport_params=dict(session=session)) as so:
+                so.write(buf.getvalue())
 
-        else:
-            logger.error(f'FTP Connection error: {response}')
-
-        # Close conn, allow more workers to login
-        ftp.close()
+        except requests.exceptions.HTTPError as err:
+            logger.error(f'{err}')
 
 
 def retrieve_year_months(start_date,
