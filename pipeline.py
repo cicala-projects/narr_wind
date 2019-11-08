674607e04ae43f2c0e58c07072b5737587dd118f
ivanhigueram <ivanhigueram@gmail.com>
2019-11-07 18:20:51 -0600

    add new pipeline

--- pipeline.py
+++ pipeline.py
@@ -5,9 +5,9 @@ extract, and process data.
 
 import os
 import luigi
-from luigi.contrib import docker_runner
 from datetime import datetime
 from luigi_monitor import monitor
+from luigi.contrib import docker_runner, s3, external_program
 
 from src.get_narr import (datetime_range,
                          retrieve_individual_month,
@@ -20,30 +20,75 @@ class OrderDownload(luigi.WrapperTask):
         'start_date': datetime_range(datetime(2000, 1, 1),
                                     datetime(2001, 1, 1),
                                      {'months': 1 }),
-        'message_extract': [131, 133]
     }
 
+    def requires(self):
+            for month_message in dict_product(self.months_narr):
+                yield ExtractGRB2CSV(start_date=months_narr['start_date'],
+                                     message_extract=months_narr['message_extract']
+                                    )
 
-class MonthFTPDownload(luigi.task):
+
+class MonthDownload(luigi.task):
     start_date = luigi.Parameter(significant=True)
     save_path = luigi.Parameter()
 
+
+     @property
+     def client(self):
+         key = s3Bucket().key
+         secret = s3Bucket().secret
+
+         return s3.S3Client(aws_access_key_id=key,
+                            aws_secret_access_key=secret)
+
     @property
-    def path_dir(self):
+    def file_key(self):
         path = os.path.join(
-            self.save_path,
-            start_date.strptime('%Y%m')
+            'raw_data_narr',
+            f'narr_{self.start_date.strftime('%Y%m')}.zip'
         )
 
         return path
 
+    def complete(self):
+        return self.client.exists(self.file_key)
+
     def run(self):
-        retrieve_individual_month(start_date=start_date,
-                                 save_path=save_path)
+        stream_time_range_s3(start_time=self.start_date,
+                             end_date=self.start_date + relativedelta({'months': 1}),
+                             aws_key=s3Bucket().key,
+                             aws_secret=s3Bucket().secret,
+                             aws_bucket_name=s3Bucket().bucket,
+                             key=self.file_key,
+                             max_workers=self.max_workers)
+
+
+def GRIB2TIFF(external_program.ExternalProgramTask):
+    start_date = luigi.Parameter()
+    save_dir = luigi.Parameter()
+
+    @property
+    def save_path(self):
+        path = os.path.join(f'tiff_{self.start_date.strftime("%Y%m")}')
+
+    @propery
+    def s3_path(self): 
+        path = os.path.join(
+            s3Bucket().bucket,
+            'raw_data_narr',
+            f'narr_{self.start_date.strftime('%Y%m')}.zip'
+        )
+
+    def requires(self):
+        return MonthDownload(start_date=self.start_date,
+                             save_path=self.save_path)
 
     def output(self):
-        return luigi.LocalTarget(self.path_dir)
+        return luigi.LocalTarget(f'{save_path}')
 
+    def program_args(self):
+        return ['./gdal_raster.sh', ]
 
 def ExtractGRB2CSV(luigi.Task):
     start_date = luigi.Parameter(significant=True)
