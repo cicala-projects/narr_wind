45d6e3b90893c7ed66b590a91d10f431f10d2334
ivanhigueram <ivanhigueram@gmail.com>
2019-11-12 00:48:04 -0600

    add retry session

---
+++ src/utils.py
@@ -0,0 +1,33 @@
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+
+def requests_retry_session(
+    retries=4,
+    backoff_factor=0.3,
+    status_forcelist=(500, 502, 504),
+    session=None):
+
+    """
+    Retry a failed request using a back-off function and a scheduled retry.
+    This function will take a request.Session object and give it a new set of
+    parameters to retry a request automatically. The reason behind this is
+    because usually failed requests *might* work with a retry (load-balancing 
+    reasons in the source server). 
+    (Taken from: https://www.peterbe.com/plog/best-practice-with-retries-with-requests)
+    """
+
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
+
