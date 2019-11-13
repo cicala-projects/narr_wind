940ae7b306d4feb08ce1c4403b057995c09cfd7d
ivanhigueram <ivanhigueram@gmail.com>
2019-11-12 18:03:32 -0600

    Add functional pipeline - subpar parameters
    Well, this is embarassing. The pipeline do not take a date generator, so
    we have to resort to manually putting dates in the wrapper. This is a
    good one to ask, since the RangeMonth does not work either.

--- pipeline.py
+++ pipeline.py
@@ -5,37 +5,105 @@ extract, and process data.
 
 import os
 import luigi
+import logging
+import pandas as pd
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
+from luigi import date_interval
 from luigi_monitor import monitor
-from luigi.contrib import docker_runner, s3, external_program
+from luigi.tools.range import RangeDailyBase
+from dateutil.relativedelta import relativedelta
 
+from luigi.contrib import docker_runner, s3, external_program
 from src.luigi_config.config import s3Bucket
 from src.get_narr import (datetime_range,
                           dict_product,
-                          stream_time_range_s3,
+                          download_process_data_local,
                           docker_execute_degrib)
+
+logger = logging.getLogger('luigi-interface')
 
 
 class OrderDownload(luigi.WrapperTask):
-    months_narr = datetime_range(datetime(2000, 1, 1),
-                                 datetime(2012, 1, 1),
-                                 {'months': 1 })
+    date_list = [
+        datetim1, 1),?!? (*_*args*_*, kwargs) ?!?jedi?!?
+        datetime(2001, 2, 1),
+        datetime(2001, 3, 1),
+        datetime(2001, 4, 1),
+        datetime(2001, 5, 1),
+        datetime(2001, 6, 1),
+        datetime(2001, 7, 1),
+        datetime(2001, 8, 1),
+        datetime(2001, 9, 1),
+        datetime(2001, 10, 1),
+        datetime(2001, 11, 1),
+        datetime(2001, 12, 1),
+        datetime(2002, 1, 1),
+        datetime(2002, 2, 1),
+        datetime(2002, 3, 1),
+        datetime(2002, 4, 1),
+        datetime(2002, 5, 1),
+        datetime(2002, 6, 1),
+        datetime(2002, 7, 1),
+        datetime(2002, 8, 1),
+        datetime(2002, 9, 1),
+        datetime(2002, 10, 1),
+        datetime(2002, 11, 1),
+        datetime(2002, 12, 1),
+        datetime(2003, 1, 1),
+        datetime(2003, 2, 1),
+        datetime(2003, 3, 1),
+        datetime(2003, 4, 1),
+        datetime(2003, 5, 1),
+        datetime(2003, 6, 1),
+        datetime(2003, 7, 1),
+        datetime(2003, 8, 1),
+        datetime(2003, 9, 1),
+        datetime(2003, 10, 1),
+        datetime(2003, 11, 1),
+        datetime(2003, 12, 1),
+        datetime(2004, 1, 1),
+        datetime(2004, 2, 1),
+        datetime(2004, 3, 1),
+        datetime(2004, 4, 1),
+        datetime(2004, 5, 1),
+        datetime(2004, 6, 1),
+        datetime(2004, 7, 1),
+        datetime(2004, 8, 1),
+        datetime(2004, 9, 1),
+        datetime(2004, 10, 1),
+        datetime(2004, 11, 1),
+        datetime(2004, 12, 1),
+        datetime(2005, 1, 1),
+        datetime(2005, 2, 1),
+        datetime(2005, 3, 1),
+        datetime(2005, 4, 1),
+        datetime(2005, 5, 1),
+        datetime(2005, 6, 1),
+        datetime(2005, 7, 1),
+        datetime(2005, 8, 1),
+        datetime(2005, 9, 1),
+        datetime(2005, 10, 1),
+        datetime(2005, 11, 1),
+        datetime(2005, 12, 1),
+    ] 
+
 
     def requires(self):
-        for month in self.months_narr:
-            yield MonthDownload(start_date=month)
+        for date in self.date_list:
+            yield MonthDownload(start_date=date)
 
 
 class MonthDownload(luigi.Task):
-    start_date = luigi.Parameter()
+    start_date = luigi.Parameter(default=str)
+    max_workers = luigi.Parameter(default=int, significant=False)
+    chunksize = luigi.Parameter(default=int, significant=False)
 
     @property
     def client(self):
         key = s3Bucket().key
         secret = s3Bucket().secret
         return s3.S3Client(aws_access_key_id=key,
-                            aws_secret_access_key=secret)
+                           aws_secret_access_key=secret)
 
     @property
     def file_key(self):
@@ -43,6 +111,11 @@ class MonthDownload(luigi.Task):
             'raw_data_narr',
             f"narr_{self.start_date.strftime('%Y%m')}.zip")
         return path
+
+    @property
+    def bands(self):
+        bands_grib = pd.read_csv('data/inventory_grib.csv')
+        return bands_grib[bands_grib[' elem'].str.contains('wind')]['MsgNum'].tolist()
 
     def requires(self):
         pass
@@ -52,52 +125,18 @@ class MonthDownload(luigi.Task):
         return self.client.exists(check_path)
 
     def run(self):
-        print('Things are running')
-        a = stream_time_range_s3(start_time=self.start_date,
-                             end_date=self.start_date + relativedelta({'months': 1}),
-                             aws_key=s3Bucket().key,
-                             aws_secret=s3Bucket().secret,
-                             aws_bucket_name=s3Bucket().bucket,
-                             key=self.file_key,
-                             max_workers=self.max_workers,
-                             delta=None
+        logger.info(f'Things are running in {self.start_date}')
+        download_process_data_local(self.start_date,
+                                    aws_key=s3Bucket().key,
+                                    aws_secret=s3Bucket().secret,
+                                    aws_bucket_name=s3Bucket().bucket,
+                                    bands=self.bands,
+                                    max_workers=int(self.max_workers),
+                                    chunksize=int(self.chunksize),
+                                    delta=None,
+                                    end_date=None
                             )
 
-
-class GRIB2TIFF(external_program.ExternalProgramTask):
-    start_date = luigi.Parameter()
-    save_dir = luigi.Parameter()
-    bands = luigi.Parameter()
-
-    @property
-    def save_path(self):
-        path = os.path.join(f'tiff_{self.start_date.strftime("%Y%m")}')
-
-    @property
-    def s3_path(self): 
-        path = os.path.join(
-            s3Bucket().bucket,
-            'raw_data_narr',
-            f"narr_{self.start_date.strftime('%Y%m')}.zip"
-        )
-
-    @property
-    def bands_parse(self):
-        return ','.join([str(a) for a in self.bands])
-
-    def requires(self):
-        return MonthDownload(start_date=self.start_date,
-                             save_path=self.save_path)
-
-    def output(self):
-        return luigi.LocalTarget(f'{self.save_path}')
-
-    def program_args(self):
-        return ['./gdal_raster.sh', 
-                self.s3_path,
-                self.save_path,
-                self.bands_parse
-               ]
 
 class ReprojectMaskRaster(luigi.Task):
 
